f4bae2eef284250f0c048ace320732ce15f94543
amaury5432 <amauryval@gmail.com>
2020-12-27 15:10:41 +0100

    upd (topo chart) style improved but not perfect

--- web/static/scripts/topo_chart.js
+++ web/static/scripts/topo_chart.js
@@ -4,8 +4,8 @@ function createTopoChart() {
 
     // Set the dimensions of the canvas / graph
     var	margin = {top: 30, right: 20, bottom: 30, left: 50},
-        width = 600 - margin.left - margin.right,
-        height = 280 - margin.top - margin.bottom;
+        width = $("#results-bar").width() - margin.left - margin.right,
+        height = $("#right-bar").height() - $("#title-bar").height() - $("#controler-bar").height() - $("#results-bar").height() - margin.top - margin.bottom;
 
     // Set the ranges
     var	x = d3.scaleLinear().range([0, width]);
@@ -33,7 +33,10 @@ function createTopoChart() {
     var data = PointPathData.features
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.properties.distance; }));
-    y.domain([d3.min(data, function(d) { return d.properties.elevation; }) - 5, d3.max(data, function(d) { return d.properties.elevation; }) + 5]);
+    y.domain([
+        d3.min(data, function(d) { return d.properties.elevation; }) - 5,
+        d3.max(data, function(d) { return d.properties.elevation; }) + 5]
+    );
 
     // Add the line_value path.
     svg.append("path")
@@ -68,6 +71,7 @@ function createTopoChart() {
             var tooltip_div = d3.select("body").append("div")
                 .attr("class", "tooltip")
                 .style("opacity", 0);
+
             tooltip_div.transition()
                 .duration(200)
                 .style("opacity", .9);
@@ -91,6 +95,9 @@ function createTopoChart() {
                 .style("opacity", "0")
                 .style("fill", "red")
         })
+        .on("mousemove", function(d) {
+            place_popup()
+        })
 
     // Add X axis
     svg.append("g")
@@ -103,6 +110,7 @@ function createTopoChart() {
       .attr("transform",
             "translate(" + (width/2) + " ," + (height + margin.top) + ")")
       .style("text-anchor", "middle")
+      .style("font-size", "10px")
       .text("Distance parcourue (mètre)");
 
     // Add Y axis
@@ -117,7 +125,32 @@ function createTopoChart() {
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
+      .style("font-size", "10px")
       .text("Altitude (mètres)");
 
 };
 
+function place_popup() {
+    // always only 1 popup
+    let current_popup = d3.select('.tooltip')
+
+    // do not change with let or you'll have value issue
+    var popup_width = $(".tooltip").width()
+    var popup_height = $(".tooltip").height()
+
+    current_popup
+    .style('left', function () {
+        if (d3.event.pageX + popup_width + 20 > $(window).width()) {
+            return d3.event.pageX - popup_width - 15 + 'px'
+        } else {
+            return d3.event.pageX + 15 + 'px'
+        }
+    })
+    .style('top', function () {
+        if (d3.event.pageY + popup_height + 20 > $(window).height()) {
+            return d3.event.pageY - popup_height - 15 + 'px'
+        } else {
+            return d3.event.pageY + 15 + 'px'
+        }
+    })
+}
