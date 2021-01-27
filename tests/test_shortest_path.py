fa07c5c6e65417d54dfe45ff5127aa6f574b61db
amauryval <amauryval@gmail.com>
2021-01-27 18:48:36 +0100

    upd (test) new outputs

--- tests/test_shortest_path.py
+++ tests/test_shortest_path.py
@@ -9,12 +9,14 @@ def test_pedestrian_path_with_elevation(path_nodes):
         mode="pedestrian",
         geojson=path_nodes,
         elevation_mode=True,
+        path_name="path1",
+        is_loop=True
     ).run()
 
     assert len(geojson_points_data["features"]) > 0
-    assert set(geojson_points_data["features"][0]["properties"].keys()) == {"height", "distance"}
+    assert set(geojson_points_data["features"][0]["properties"].keys()) == {'height', 'path_name', 'distance', 'position', 'uuid'}
     assert len(geojson_line_data["features"]) > 0
-    assert set(geojson_line_data["features"][0]["properties"].keys()) == {"source_node", "target_node", "path_step", "length"}
+    assert set(geojson_line_data["features"][0]["properties"].keys()) == {'target_node', 'path_name', 'path_position', 'length', 'source_node'}
     assert set(statistics.keys()) == {"height_diff", "height_max", "height_min", "nodes_count", "length"}
     assert sum(statistics.values()) > 0
 
@@ -25,12 +27,14 @@ def test_vehicle_path_with_elevation(path_nodes):
         mode="vehicle",
         geojson=path_nodes,
         elevation_mode=True,
+        path_name="path1",
+        is_loop=True
     ).run()
 
     assert len(geojson_points_data["features"]) > 0
-    assert set(geojson_points_data["features"][0]["properties"].keys()) == {"height", "distance"}
+    assert set(geojson_points_data["features"][0]["properties"].keys()) == {'height', 'path_name', 'distance', 'position', 'uuid'}
     assert len(geojson_line_data["features"]) > 0
-    assert set(geojson_line_data["features"][0]["properties"].keys()) == {"source_node", "target_node", "path_step", "length"}
+    assert set(geojson_line_data["features"][0]["properties"].keys()) == {'target_node', 'path_name', 'path_position', 'length', 'source_node'}
     assert set(statistics.keys()) == {"height_diff", "height_max", "height_min", "nodes_count", "length"}
     assert sum(statistics.values()) > 0
 
@@ -40,13 +44,15 @@ def test_pedestrian_path_without_elevation(path_nodes):
     geojson_points_data, geojson_line_data, statistics = ComputePath(
         mode="pedestrian",
         geojson=path_nodes,
-        elevation_mode=False
+        elevation_mode=False,
+        path_name="path1",
+        is_loop=True
     ).run()
 
     assert len(geojson_points_data["features"]) > 0
-    assert set(geojson_points_data["features"][0]["properties"].keys()) == {"height", "distance"}
+    assert set(geojson_points_data["features"][0]["properties"].keys()) == {'height', 'path_name', 'distance', 'position', 'uuid'}
     assert len(geojson_line_data["features"]) > 0
-    assert set(geojson_line_data["features"][0]["properties"].keys()) == {"source_node", "target_node", "path_step", "length"}
+    assert set(geojson_line_data["features"][0]["properties"].keys()) == {'target_node', 'path_name', 'path_position', 'length', 'source_node'}
     assert set(statistics.keys()) == {"height_diff", "height_max", "height_min", "nodes_count", "length"}
     assert sum(statistics.values()) > 0
 
@@ -56,13 +62,15 @@ def test_vehicle_path_without_elevation(path_nodes):
     geojson_points_data, geojson_line_data, statistics = ComputePath(
         mode="vehicle",
         geojson=path_nodes,
-        elevation_mode=False
+        elevation_mode=False,
+        path_name="path1",
+        is_loop=False
     ).run()
 
     assert len(geojson_points_data["features"]) > 0
-    assert set(geojson_points_data["features"][0]["properties"].keys()) == {"height", "distance"}
+    assert set(geojson_points_data["features"][0]["properties"].keys()) == {'height', 'path_name', 'distance', 'position', 'uuid'}
     assert len(geojson_line_data["features"]) > 0
-    assert set(geojson_line_data["features"][0]["properties"].keys()) == {"source_node", "target_node", "path_step", "length"}
+    assert set(geojson_line_data["features"][0]["properties"].keys()) == {'target_node', 'path_name', 'path_position', 'length', 'source_node'}
     assert set(statistics.keys()) == {"height_diff", "height_max", "height_min", "nodes_count", "length"}
     assert statistics.pop("length") > 0
     assert statistics.pop("nodes_count") == 3
